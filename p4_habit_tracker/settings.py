18817a62a369b9fea3ab9868c6e1bc83918570ca
Aisha <aishie_hk@icloud.com>
2025-06-17 01:38:53 +0100

    Add Heroku domain to ALLOWED_HOSTS

--- p4_habit_tracker/settings.py
+++ p4_habit_tracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['p4-habit-tracker-535b0b8611ff.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
